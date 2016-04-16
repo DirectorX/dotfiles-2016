a3cf5680083c42ba874ecebc0a3ea8de98fedfd4
DirectorX <DirectorX@users.noreply.github.com>
2016-04-16 22:39:59 +0300

    added rofi launcher

---
+++ i3/bin/rofi.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rofi -color-window "#273238, #273238, #1e2529" -color-normal "#273238, #c1c1c1, #273238, #394249, #ffffff" -color-active "#273238, #80cbc4, #273238, #394249, #80cbc4" -color-urgent "#273238, #ff1844, #273238, #394249, #ff1844"  -width -30 "$@"
