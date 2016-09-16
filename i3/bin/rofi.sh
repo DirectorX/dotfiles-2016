9bc106b8743e5819f27cfaa169938f94a0869cf5
DirectorX <DirectorX@users.noreply.github.com>
2016-09-16 13:11:13 +0300

    update rofi

--- i3/bin/rofi.sh
+++ i3/bin/rofi.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
-rofi -color-window "#273238, #273238, #1e2529" -color-normal "#273238, #c1c1c1, #273238, #394249, #ffffff" -color-active "#273238, #80cbc4, #273238, #394249, #80cbc4" -color-urgent "#273238, #ff1844, #273238, #394249, #ff1844"  -width -30 "$@"
+rofi -width `xrandr | head -n 1 | sed -r 's/.*current\s([0-9]+)\sx\s([0-9]+).*/\1x\2/g' | cut -d 'x' -f 1` \
+     -color-window "#273238, #273238, #1e2529" \
+     -color-normal "#273238, #c1c1c1, #273238, #394249, #ffffff" \
+     -color-active "#273238, #80cbc4, #273238, #394249, #80cbc4" \
+     -color-urgent "#273238, #ff1844, #273238, #394249, #ff1844" \
+     -location 0 -lines 8 -bw 0 -font "Source Code Pro Bold 16" \
+     -padding 400 -separator-style none -opacity 85 "$@"
